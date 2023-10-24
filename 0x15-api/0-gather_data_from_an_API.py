361bd4aa9b38a281ef16db3788d9e20f0075d9f0
aabine <kendoaustine@yahoo.com>
2023-10-24 23:01:10 +0100

    small fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,24 +16,24 @@ def get_employee_todo_progress(employee_id):
     if not isinstance(employee_id, int):
         raise ValueError("Employee ID must be an integer.")
 
-    # Retrieve user data
-    user_data = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
+    # Get content from Api response
+    url = "https://jsonplaceholder.typicode.com/"
+    get_user = requests.get(url + "users/{}".format(employee_id))
+    get_tasks = requests.get(f"{url}todos", params={"userId": employee_id})
 
-    # Retrieve completed tasks for the employee
-    get_content_from_user = requests.get(
-        "https://jsonplaceholder.typicode.com/todos",
-        params={"userId": employee_id}).json()
+    # Get data from response
+    user_data = get_user.json()
+    tasks_data = get_tasks.json()
 
-    # Filter completed tasks
-    completed_tasks = [task for task in get_content_from_user
-                       if task["completed"]]
+    # Get completed tasks
+    completed_tasks = [task for task in tasks_data if task["completed"]]
 
-    # Print employee progress
+    # Print completed tasks
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data["name"], len(completed_tasks), len(get_content_from_user)))
+        user_data["name"], len(completed_tasks), len(tasks_data)
+    ))
     for task in completed_tasks:
-        print(f"\t{task['title']}")
+        print("\t {}".format(task["title"]))
 
 
 if __name__ == "__main__":
