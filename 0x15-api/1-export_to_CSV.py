9a04897b6ba578fe17c725c8714e23c191981a50
aabine <kendoaustine@yahoo.com>
2023-10-24 06:15:06 +0100

    small fixes

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,23 +4,24 @@ import csv
 import requests
 import sys
 
+
 def export_user_tasks_to_csv(user_id):
     # Base URL for the JSONPlaceholder API
     base_url = "https://jsonplaceholder.typicode.com/"
-    
     # Make a request to get the user's data
     user = requests.get(base_url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    
     # Make a request to get the user's to-do list
     todos = requests.get(base_url + "todos", params={"userId": user_id}).json()
-    
     # Create and write to the CSV file
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        
         # Use a list comprehension to write each task's information
-        [writer.writerow([user_id, username, t.get("completed"), t.get("title")]) for t in todos]
+        [writer.writerow([user_id,
+                          username,
+                          t.get("completed"),
+                          t.get("title")]) for t in todos]
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
